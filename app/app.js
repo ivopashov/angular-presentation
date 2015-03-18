837be18a9915307d5f27140f63e1c50799e25275
ivaylo pashov <ivopashov@abv.bg>
2015-03-18 23:34:55 +0200

    initial commit

--- app/app.js
+++ app/app.js
@@ -3,10 +3,13 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.wizard',
+  'myApp.thirdPartyComponent',
+  'myApp.directiveInheritedScope',
+  'myApp.isolatedScope',
+  'myApp.testTable',
+  'myApp.date'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/wizard'});
 }]);
